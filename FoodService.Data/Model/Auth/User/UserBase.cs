8740b21300decb5762b532f0f156a113c96d26d9
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2024-05-02 16:44:52 -0300

    FIX: removing unused field 'Nome' in UserBase.cs

--- FoodService.Data/Model/Auth/User/UserBase.cs
+++ FoodService.Data/Model/Auth/User/UserBase.cs
@@ -8,11 +8,6 @@ namespace FoodService.Data.Model.Auth.User
     public class UserBase : IdentityUser<int>
     {
         /// <summary>
-        /// Gets or sets the name of the user.
-        /// </summary>
-        public string? Nome { get; set; }
-
-        /// <summary>
         /// Gets or sets the discriminator of the user.
         /// </summary>
         public string? Discriminator { get; set; }
